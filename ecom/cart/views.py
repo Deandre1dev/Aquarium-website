4a471fc23ba3330ba0437cb2472301214b096f3e
Deandre <deandrewright333@gmail.com>
2025-10-23 16:06:55 +0200

    Added a cart quantity function to display when a user adds a product.

--- ecom/cart/views.py
+++ ecom/cart/views.py
@@ -14,17 +14,27 @@ def cart_add(request):
     if request.POST.get('action') == 'post':
         # Get information
         product_id = int(request.POST.get('product_id'))
+
         # Lookup product in the DB
         product = get_object_or_404(Product, id=product_id)
+
         # Save to a session
         cart.add(product=product)
-        # Return a json response
-        response = JsonResponse({'Product Name': product.name})
+
+        # Get Cart quantity.
+        cart_quantity = cart.__len__()
+
+        # Return a json response to send back data to the webpage.
+        #response = JsonResponse({'Product Name': product.name})
+        
+        response = JsonResponse({'qty': cart_quantity})
         # Return the response
         return response
 
 def cart_delete(request):
+    #Need to add functionality
     pass
 
 def cart_update(request):
+    #Need to add functionality#
     pass
